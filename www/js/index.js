05341521ed540f009d377ad554da5ca7c6cc12eb
fabio franci <fabio.franci@gmail.com>
2015-11-03 04:15:43 +0100

    Vai

--- www/js/index.js
+++ www/js/index.js
@@ -16,49 +16,28 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var db;
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-
-        db = window.openDatabase("LaSaettaDB", "1.0", "La Saetta DB", 2000000);
-
-        alert("Ecco aperto il db");
-
-        $("#homeclienti").html('Ancora niente');
-
-
-
-
-
-
-
-
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+$(document).ready(function() {
+    // are we running in native app or in a browser?
+    window.isphone = false;
+    if(document.URL.indexOf("http://") === -1
+        && document.URL.indexOf("https://") === -1) {
+        window.isphone = true;
     }
-};
+
+    if( window.isphone ) {
+        document.addEventListener("deviceready", onDeviceReady, false);
+    } else {
+        onDeviceReady();
+    }
+});
+
+function onDeviceReady() {
+    // do everything here.
+    var db;
+    db = window.openDatabase("LaSaettaDB", "1.0", "La Saetta DB", 2000000);
+
+    alert("Ecco aperto il db");
+
+    $("#homeclienti").html('Ancora niente');
+}
+

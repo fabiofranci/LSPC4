d4363b65aa2568ad9484511f49d2e1f540453909
fabio franci <fabio.franci@gmail.com>
2015-11-15 11:02:16 +0100

    Vai

--- www/js/index.js
+++ www/js/index.js
@@ -345,14 +345,15 @@ function onDeviceReady() {
                 clienti_server = data.items;
                 var i=0;
                 $.each(clienti_server, function (index, cliente) {
+                    alert('cliente numero '+i+' --> id='+cliente.id+' nome='+cliente.nome_o_ragione_sociale);
                     if (i==0) {
                         rigaselect="INSERT OR REPLACE INTO SERVER_CLIENTI (id, nome_o_ragione_sociale, partita_iva, codice_fiscale, tipo, persona_di_riferimento, telefono, email, note) SELECT '"+cliente.id+"' AS id, '"+cliente.nome_o_ragione_sociale+"' AS nome_o_ragione_sociale, '"+cliente.partita_iva+"' as partita_iva, '"+cliente.codice_fiscale+"' AS codice_fiscale,'"+cliente.tipo+"' AS tipo, '"+cliente.persona_di_riferimento+"' AS persona_di_riferimento,'"+cliente.telefono+"' AS telefono, '"+cliente.email+"' AS email,'"+cliente.note+"' AS note  ";
                     } else {
-                        rigaselect+=" UNION ALL SELECT '"+clienti.id+"','"+clienti.nome_o_ragione_sociale+"','"+clienti.partita_iva+"','"+clienti.codice_fiscale+"','"+clienti.persona_di_riferimento+"','"+clienti.telefono+"','"+clienti.email+"','"+clienti.note+"'";
+                        rigaselect+=" UNION ALL SELECT '"+cliente.id+"','"+cliente.nome_o_ragione_sociale+"','"+cliente.partita_iva+"','"+cliente.codice_fiscale+"','"+cliente.persona_di_riferimento+"','"+cliente.telefono+"','"+cliente.email+"','"+cliente.note+"'";
                     }
                 });
-                //alert(rigaselect);
-                console.log(rigaselect);
+                alert(rigaselect);
+                //console.log(rigaselect);
                 //ora può lanciare la transazione
                 db.transaction(
                     function (tx3) { tx3.executeSql(rigaselect); },
